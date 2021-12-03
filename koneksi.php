9957ef4ac503574eb2c95f159e45a47ef06768a6
Reza Alfajriansyah <75005749+Reza-Alfajri@users.noreply.github.com>
2021-12-03 09:42:46 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,14 @@
+<?php 
+    // Nama File : koneksi.php
+    //angga cobe cobe
+    $server = "localhost";
+    $user = "root";
+    $password = "";
+    $nama_database = "lpmp";
+
+    $db = mysqli_connect($server, $user, $password, $nama_database);
+
+    if(!$db){
+        die("Gagal terhubung dengan database: ". mysqli_connect_error());
+    }
+?>
