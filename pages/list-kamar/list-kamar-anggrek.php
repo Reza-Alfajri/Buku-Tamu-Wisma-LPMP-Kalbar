de50bb0b2c3684bf05e431f49fcc3952ce9a7dd4
Reza Alfajriansyah <75005749+Reza-Alfajri@users.noreply.github.com>
2021-12-09 10:44:50 +0700

    Ubah tampilan kolum time stamp
    Jadi jam doang

--- pages/list-kamar/list-kamar-anggrek.php
+++ pages/list-kamar/list-kamar-anggrek.php
@@ -193,8 +193,8 @@
                                 //$no++; 
                                 ?> 
                                 <tr class="alert" role="alert">
-				                            <td><?= $list['nomor_kamar']; ?></td>
-                                    <td><?= $list['timestamp']; ?></td>
+				    <td><?= $list['nomor_kamar']; ?></td>
+                                    <td><?= date_format(date_create($list['timestamp']),"H:i"); ?></td>
                                     <td><?= $list['nama_kegiatan']; ?></td>
                                     <td><?= $list['tanggal_awal']; ?></td>
                                     <td><?= $list['tanggal_akhir']; ?></td>
