9c236fc39f54fba233cc831ffab325d2c250fa9e
Bramantio Alif Perdana <90162706+Brmsss@users.noreply.github.com>
2021-12-10 14:01:03 +0700

    Update biodata-pengunjung.php

--- pages/biodata/biodata-pengunjung.php
+++ pages/biodata/biodata-pengunjung.php
@@ -6,6 +6,11 @@
 <html lang="en">
 
 <head>
+  <style>
+    .swal2-popup {
+      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+  </style>
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
