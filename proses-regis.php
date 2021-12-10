fb17ee157b4ae3854dd9bffce85c17f317e4fd43
Bramantio Alif Perdana <90162706+Brmsss@users.noreply.github.com>
2021-12-10 13:48:29 +0700

    Update proses-regis.php

--- proses-regis.php
+++ proses-regis.php
@@ -1,23 +1,64 @@
+<html>
+<head>
+<!-- sweet alert --> 
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script> 
+<!-- end sweet alert --> 
+</head>
+<body>
 <?php
     //Nama File : proses_registrasi.php
     //menkoneksikan ke database
     include("koneksi.php");
     //cek apakah tombol daftar sudah diklik atau belum
-    if(isset($_POST['daftar'])) {
-        //ambil data dari formulir
         $username = $_POST['username'];
         $password = $_POST['password'];
+        $repassword = $_POST['re-password'];
         //buat query
-        $sql = "INSERT INTO admin (username, password)
-        VALUE ('$username', '$password')";
-        $query = mysqli_query($db, $sql);
-        //apakah query simpan berhasil?
-        if ($query) {
-            header('Location: login.php?status=sukses');
+        if ($password<>$repassword) {   
+            echo '<script type="text/javascript">
+              Swal.fire({
+                title: "Password dan Konfirmasi Password Tidak Sama!",
+                icon: "warning",
+                timer: 1500,
+                didOpen: () => {
+                Swal.showLoading()
+                const b = Swal.getHtmlContainer().querySelector("b")
+                    timerInterval = setInterval(() => {
+                    b.textContent = Swal.getTimerLeft()
+                    }, 100)
+                },
+                willClose: () => {
+                    clearInterval(timerInterval)
+                }
+            }).then((result) => {
+              /* Read more about handling dismissals below */
+              if (result.dismiss === Swal.DismissReason.timer) {
+                window.history.back();
+              }
+            });
+            </script>';
         } else {
-            header('Location: login.php?status=gagal');
+            $sql = "INSERT INTO admin (username, password)
+            VALUE ('$username', '$password')";
+            $query = mysqli_query($db, $sql);
+            if ($query) {
+                echo '
+                <script language="javascript">
+                Swal.fire({
+                    title: "Registrasi Sukses",
+                    icon: "success",
+                    confirmButtonText: "Login",
+                  }).then((result) => {
+                    if (result.isConfirmed) {
+                        window.location.href="login.php?status=berhasil";
+                    }
+                  }); 
+                </script>';
+            } else {
+                header('Location: regis.php?status=gagal');
+            }
+            
         }
-    } else {
-        die("Akses dilarang...");
-    }
 ?>
+</body>
+</html>
