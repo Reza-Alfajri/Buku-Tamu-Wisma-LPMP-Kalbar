fc9065af6d61ba8fc229991027b8d9f1e4a846fd
Sahdiki <79701880+sahdikigg@users.noreply.github.com>
2021-12-13 09:26:05 +0700

    ganti icon tab

--- login.php
+++ login.php
@@ -18,7 +18,7 @@
     <!-- inject:css -->
     <link rel="stylesheet" href="css/vertical-layout-light/style.css" />
     <!-- endinject -->
-    <link rel="shortcut icon" href="images/favicon.png" />
+    <link rel="shortcut icon" href="images/logo-lpmp-kecil.png" />
     <!-- Bootstrap 4 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <!-- Font Awesome -->
