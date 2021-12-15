9602012f418ea0d710e1753f60bf8387b462feb9
Bramantio Alif Perdana <90162706+Brmsss@users.noreply.github.com>
2021-12-15 11:17:32 +0700

    Update rekapan.php

--- pages/rekap/rekapan.php
+++ pages/rekap/rekapan.php
@@ -1,332 +1,37 @@
 <?php
-	session_start();
-	if(!isset($_SESSION["username"])){
-	    header("Location: login.php");
-	}
-	require '../../koneksi.php';
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Rekap Tamu Wisma</title>
-  <!-- plugins:css -->
-  <link rel="stylesheet" href="../../vendors/feather/feather.css">
-  <link rel="stylesheet" href="../../vendors/ti-icons/css/themify-icons.css">
-  <link rel="stylesheet" href="../../vendors/css/vendor.bundle.base.css">
-  <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-  <!-- endinject -->
-  <!-- Plugin css for this page -->
-  <link rel="stylesheet" href="../../vendors/datatables.net-bs4/dataTables.bootstrap4.css">
-  <link rel="stylesheet" href="../../vendors/ti-icons/css/themify-icons.css">
-  <link rel="stylesheet" type="../../text/css" href="../../js/select.dataTables.min.css">
-  <!-- End plugin css for this page -->
-  <!-- inject:css -->
-  <link rel="stylesheet" href="../../css/vertical-layout-light/style.css">
-  <!-- endinject -->
-  <link rel="shortcut icon" href="../../images/favicon.png" />
-
-  <style>
-    .swal2-popup {
-      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    include("koneksi.php");
+    //pengecekan
+    if(isset($_GET['nomor_kamar'])){
+        //ambil data dari formulir
+        $nomor_kamar = $_GET['nomor_kamar'];
+        $timestamp = "";
+        $nama_kegiatan = "";
+        $tanggal_awal = "";
+        $tanggal_akhir = "";
+        $nik = "";
+        $jenis_kelamin = "";
+        $tanggal_lahir = "";
+        $nama_tamu = "";
+        $statusco = "kosong";
+        $kota = "";
+        $jabatan = "";
+        $nama_kantor = "";
+        $no_hp = "";
+        $keterangan = "Check Out";
+        
+        //buat query update
+        $sql = "UPDATE anggrek SET timestamp='$timestamp', nama_kegiatan='$nama_kegiatan', tanggal_awal='$tanggal_awal', tanggal_akhir='$tanggal_akhir',
+        nik='$nik', jenis_kelamin='$jenis_kelamin', tanggal_lahir='$tanggal_lahir', nama_tamu='$nama_tamu', statusco='$statusco', kota='$kota', jabatan='$jabatan', nama_kantor='$nama_kantor', no_hp='$no_hp' WHERE nomor_kamar='$nomor_kamar'";
+        $sql1 = "UPDATE rekapan SET keterangan='$keterangan' where nomor_kamar='$nomor_kamar'";
+        $query = mysqli_query($db, $sql);
+        $query1 = mysqli_query($db, $sql1);
+        //apakah query berhasil tersimpan
+        if( $query && $query1 ){
+            header('Location: pages/list-kamar/list-kamar-anggrek.php');
+        } else {
+            die("Gagal menyimpan perubahan...");
+        }
+    } else {
+        die("Akses dilarang ...");
     }
-  </style>
-</head>
-<body>
-  <div class="container-scroller">
-    <!-- partial:partials/_navbar.html -->
-    <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
-      <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-        <a class="navbar-brand brand-logo ms-10" href="../../index.php"><img src="../../images/logo-lpmp.png" alt="logo"/></a>
-        <a class="navbar-brand d-lg-none" href="../../index.php"><img src="../../images/logo-lpmp-kecil.png" width="36px" height="40px" alt="logo"/></a>
-      </div>
-      <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
-        <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
-          <span class="icon-menu"></span>
-        </button>
-    
-        <!-- End Search -->
-        <!-- Start Notif -->
-        <ul class="navbar-nav navbar-nav-right">
-          
-        </ul>
-        <!-- End Notif -->
-        <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
-          <span class="icon-menu"></span>
-        </button>
-      </div>
-    </nav>
-    <!-- partial -->
-    <div class="container-fluid page-body-wrapper">
-      <!-- partial:partials/_sidebar.html -->
-      <nav class="sidebar sidebar-offcanvas" id="sidebar">
-        <ul class="nav">
-          <!-- Home -->
-          <li class="nav-item">
-            <a class="nav-link" href="../../index.php">
-              <i class="icon-grid menu-icon"></i>
-              <span class="menu-title">Home</span>
-            </a>
-          </li>
-          <!-- Biodata -->
-          <li class="nav-item">
-            <a class="nav-link" href="../../pages/biodata/biodata-pengunjung.php">
-              <i class="icon-paper menu-icon"></i>
-              <span class="menu-title">Biodata Pengunjung</span>
-            </a>
-          </li>
-          <!--  -->
-          <!-- List Kamar -->
-          <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-              <i class="icon-layout menu-icon"></i>
-              <span class="menu-title">List Kamar</span>
-              <i class="menu-arrow"></i>
-            </a>
-            <div class="collapse" id="ui-basic">
-              <ul class="nav flex-column sub-menu">
-                <li class="nav-item"> <a class="nav-link" href="../../pages/list-kamar/list-kamar-handayani.php">Handayani</a></li>
-                <li class="nav-item"> <a class="nav-link" href="../../pages/list-kamar/list-kamar-anggrek.php">Anggrek</a></li>
-              </ul>
-            </div>
-          </li>
-          <!-- Rekapan -->
-          <li class="nav-item active">
-            <a class="nav-link" href="../../pages/rekap/rekapan.php">
-              <i class="ti-agenda menu-icon"></i>
-              <span class="menu-title">Rekapan Pengunjung</span>
-            </a>
-          </li>
-          <!-- Logout -->
-          <li class="nav-item">
-            <a class="nav-link" href="../../logout.php" onclick="return logout(event)">
-              <i class="fa fa-sign-out menu-icon"></i>
-              <span class="menu-title">Logout</span>
-            </a>
-          </li>
-          <script type="text/javascript">
-            function logout(ev){
-              ev.preventDefault();
-              var urlToRedirect = ev.currentTarget.getAttribute('href'); 
-              console.log(urlToRedirect);
-              Swal.fire({
-              title: 'Yakin akan keluar?',
-              icon: "question",
-              showCancelButton: true,
-              confirmButtonText: 'Ya',
-              }).then((result) => {
-              if (result.isConfirmed) {
-                window.location.href = urlToRedirect;
-              }
-              })
-            }
-          </script>
-          <!--  -->
-        </ul>
-      </nav>
-      <!-- partial -->
-      <div class="main-panel">
-        <div class="content-wrapper">
-          <div class="row">
-            <div class="col-md-12 grid-margin">
-              <div class="row">
-                <div class="col-12 col-xl-8 mb-4 mb-xl-0">
-                  <h3 class="font-weight-bold fs-30">Tabel Rekap Tamu Wisma Anggrek & Handayani</h3>
-                </div>
-              </div> 
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-12 grid-margin">
-              <div class="card">
-                <div class="card-body">
-                  <h4 class="card-title">Rekap Tamu Wisma</h4>
-                    <!-- Kodingan Isi Halaman Page Di Bawah Ini -->
-                    <!-- Tabel -->
-                    <div class="row">
-                      <div class="col-md-12" style="overflow-x: auto">
-                        <div class="table-responsive" style="width:auto">
-                          <table class="table">
-                              <thead class="thead-dark">
-                                <tr>
-                                <th>Timestamp</th>
-                                <th>Nama Kegiatan</th>
-                                <th>Tanggal Awal</th>
-                                <th>Tanggal Akhir</th>
-                                <th>Nama Tamu</th>
-                                <th>NIK</th>
-                                <th>Jenis Kelamin</th>
-                                <th>Tanggal Lahir</th>
-                                <th>Kabupaten / Kota</th>
-                                <th>Jabatan</th>
-                                <th>Nama Kantor</th>
-                                <th>No. HP</th>
-                                <th>Wisma</th>
-                                <th>No. Kamar</th>
-                                </tr>
-                              </thead>
-                              <tbody>
-                                <!-- Search -->
-                                <form method="GET" class="form-inline" action="">
-                                  <div class="row">
-                                    <!-- Bulan -->
-                                    <div class="col-md-6">
-                                      <div class="form-group row">
-                                        <label class="col-sm-3 col-form-label">Bulan</label>
-                                        <div class="col-sm-9">
-                                          <select name="tanggal" class="custom-select" required="required">
-                                              <option value="">Bulan</option>
-                                              <option value="1">Januari</option>
-                                              <option value="2">Februari</option>
-                                              <option value="3">Maret</option>
-                                              <option value="4">April</option>
-                                              <option value="5">Mei</option>
-                                              <option value="6">Juni</option>
-                                              <option value="7">Juli</option>
-                                              <option value="8">Agustus</option>
-                                              <option value="9">September</option>
-                                              <option value="10">Oktober</option>
-                                              <option value="11">November</option>
-                                              <option value="12">Desember</option>
-                                            </select>
-                                        </div>
-                                      </div>
-                                    </div>
-                                    <!-- Tahun -->
-                                    <div class="col-md-6">
-                                      <div class="form-group row">
-                                        <label class="col-sm-3 col-form-label">Tahun</label>
-                                        <div class="col-sm-9">
-                                          <select name="tahun" class="custom-select" required="required">
-                                            <?php
-                                              $mulai= date('Y') - 50;
-                                              for($i = $mulai;$i<$mulai + 100;$i++){
-                                                  $sel = $i == date('Y') ? ' selected="selected"' : '';
-                                                  echo '<option value="'.$i.'"'.$sel.'>'.$i.'</option>';
-                                              }
-                                            ?>
-                                          </select>
-                                        </div>
-                                      </div>
-                                    </div>
-                                  </div>
-                                  <button class="btn btn-primary" name="filter"><span class="glyphicon glyphicon-search"></span> Search</button>
-                                  <br>
-                                  <br>
-                                </form>
-                                <?php
-                                  $hal=10;
-                                  $page=isset($_GET['hal'])?(int)$_GET['hal']:1;
-                                  $start=($page>1)?($page*$hal)-$hal:0;
-                                  
-                                  if(isset($_GET['tanggal'])){
-                                    $date = $_GET['tanggal'];
-                                    $tahun = $_GET['tahun'];
-                                    $nama_bulan = array('','Januari','Februari','Maret','April','Mei','Juni','Juli','Agustus','September','Oktober','November','Desember');
-                                    echo "Rekap Tamu Wisma Bulan ".$nama_bulan[$date]." Tahun ".$tahun;
-                                    $sql = "SELECT * FROM rekapan WHERE MONTH(tanggal_awal) = '$date' and YEAR(tanggal_awal) = '$tahun'";
-                                    $sql1 = "SELECT * FROM rekapan WHERE MONTH(tanggal_awal) = '$date' and YEAR(tanggal_awal) = '$tahun' limit $start,$hal";
-                                  } else {
-                                    $sql = "SELECT * FROM rekapan";
-                                    $sql1 = "SELECT * FROM rekapan limit $start,$hal";
-                                  }
-                                    $query = mysqli_query($db, $sql);
-                                    $query1 = mysqli_query($db, $sql1);
-                                    $total = mysqli_num_rows($query);
-                                    $pages = ceil($total/$hal);
-                                    $no = $start + 1;
-                                ?>
-                                <!-- End Search -->
-                                <?php while($list=mysqli_fetch_array($query1)) : 
-                                  //$no++; 
-                                ?> 
-                                <tr class="alert" role="alert">
-                                    <td><?= $list['timestamp']; ?></td>
-                                    <td><?= $list['nama_kegiatan']; ?></td>
-                                    <td><?= $list['tanggal_awal']; ?></td>
-                                    <td><?= $list['tanggal_akhir']; ?></td>
-                                    <td><?= $list['nama_tamu']; ?></td>
-                                    <td><?= $list['nik']; ?></td>
-                                    <td><?= $list['jenis_kelamin']; ?></td>
-                                    <td><?= $list['tanggal_lahir']; ?></td>
-                                    <td><?= $list['kota']; ?></td>
-                                    <td><?= $list['jabatan']; ?></td>
-                                    <td><?= $list['nama_kantor']; ?></td>
-                                    <td><?= $list['no_hp']; ?></td>
-                                    <td><?= $list['wisma']; ?></td>
-                                    <td><?= $list['nomor_kamar']; ?></td>
-                                </tr>
-                                <?php endwhile; ?>
-                            </tbody>
-                        </table>
-                        <!-- Pagination -->
-			                  <?php for( $i = 1 ; $i <= $pages ; $i++ ) : ?>
-                                <?php if(isset($_GET["cari"])) : ?>
-                                    <a href="?cari=<?= $_GET['cari']; ?>&hal=<?= $i; ?>">
-                                    <?= $i; ?></a>
-                                <?php else : ?>
-                                    <a href="?&hal=<?= $i; ?>">
-                                    <?= $i; ?></a>
-                                <?php endif; ?>        
-                        <?php endfor; ?>
-                        <p>Total Data : <?= mysqli_num_rows($query); ?></p>
-                        <a href="print.php?">Cetak PDF</a>
-                        </div>
-                      </div>
-                    </div>
-                    <!-- End Tabel -->
-                </div>
-              </div>
-            </div>
-          </div>
-        <!-- content-wrapper ends -->
-        <!-- partial:partials/_footer.html -->
-        <footer class="footer">
-          <div class="d-sm-flex justify-content-center justify-content-sm-between">
-            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2021.  Premium <a href="https://www.bootstrapdash.com/" target="_blank">Bootstrap admin template</a> from BootstrapDash. All rights reserved.</span>
-            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="ti-heart text-danger ml-1"></i></span>
-          </div>
-          <div class="d-sm-flex justify-content-center justify-content-sm-between">
-            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Distributed by <a href="https://www.themewagon.com/" target="_blank">Themewagon</a></span> 
-            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Edited by interns from Polnep | 2021</span>
-	  </div>
-        </footer> 
-        <!-- partial -->
-      </div>
-      <!-- main-panel ends -->
-    </div>   
-    <!-- page-body-wrapper ends -->
-  </div>
-  <!-- container-scroller -->
-
-  <!-- plugins:js -->
-  <script src="../../vendors/js/vendor.bundle.base.js"></script>
-  <!-- endinject -->
-
-  <!-- Plugin js for this page -->
-  <script src="../../vendors/chart.js/Chart.min.js"></script>
-  <script src="../../vendors/datatables.net/jquery.dataTables.js"></script>
-  <script src="../../vendors/datatables.net-bs4/dataTables.bootstrap4.js"></script>
-  <script src="../../js/dataTables.select.min.js"></script>
-  <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-  <!-- End plugin js for this page -->
-  
-  <!-- inject:js -->
-  <script src="../../js/off-canvas.js"></script>
-  <script src="../../js/hoverable-collapse.js"></script>
-  <script src="../../js/template.js"></script>
-  <script src="../../js/settings.js"></script>
-  <script src="../../js/todolist.js"></script>
-  <!-- endinject -->
-  <!-- Custom js for this page-->
-  <script src="../../js/dashboard.js"></script>
-  <script src="../../js/Chart.roundedBarCharts.js"></script>
-  <!-- End custom js for this page-->
-  
-</body>
-</html>
+?>
