6a59ab5d799cbea20213b598d4d22b2132234bb2
Sahdiki <79701880+sahdikigg@users.noreply.github.com>
2021-12-15 09:30:43 +0700

    Update rekapan.php

--- pages/rekap/rekapan.php
+++ pages/rekap/rekapan.php
@@ -136,7 +136,7 @@
             <div class="col-md-12 grid-margin">
               <div class="row">
                 <div class="col-12 col-xl-8 mb-4 mb-xl-0">
-                  <h2 class="font-weight-bold">Tabel Rekap Tamu Wisma Anggrek & Handayani</h2>
+                  <h3 class="font-weight-bold fs-30">Tabel Rekap Tamu Wisma Anggrek & Handayani</h3>
                 </div>
               </div> 
             </div>
