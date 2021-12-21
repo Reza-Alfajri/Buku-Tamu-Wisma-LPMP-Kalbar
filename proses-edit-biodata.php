323e0bb4ee82ca5e3991dc9764ff8ede76f2a1ad
AdrianSolihin <90122467+AdrianSolihin@users.noreply.github.com>
2021-12-21 10:10:10 +0700

    terbaru

--- proses-edit-biodata.php
+++ proses-edit-biodata.php
@@ -4,6 +4,7 @@
     if(isset($_POST['simpan'])){
         //ambil data dari formulir
         $nik = $_POST['nik'];
+        $nuptk = $_POST['nuptk'];
         $jenis_kelamin = $_POST['jenis_kelamin'];
         $tanggal_lahir = $_POST['tanggal_lahir'];
         $nama_tamu = $_POST['nama_tamu'];
@@ -13,14 +14,30 @@
         $no_hp = $_POST['no_hp'];
         
         //buat query update
-        $sql1 = "UPDATE biodata SET nik='$nik', jenis_kelamin='$jenis_kelamin', tanggal_lahir='$tanggal_lahir', nama_tamu='$nama_tamu',
+        $sql1 = "UPDATE biodata SET nik='$nik', nuptk='$nuptk', jenis_kelamin='$jenis_kelamin', tanggal_lahir='$tanggal_lahir', nama_tamu='$nama_tamu',
         kota='$kota', jabatan='$jabatan', nama_kantor='$nama_kantor', no_hp='$no_hp' WHERE nik='$nik'";
-        $sql2 = "UPDATE rekapan SET nik='$nik', jenis_kelamin='$jenis_kelamin', tanggal_lahir='$tanggal_lahir', nama_tamu='$nama_tamu',
+
+        $sql2 = "UPDATE rekapan SET nik='$nik', nuptk='$nuptk', jenis_kelamin='$jenis_kelamin', tanggal_lahir='$tanggal_lahir', nama_tamu='$nama_tamu',
         kota='$kota', jabatan='$jabatan', nama_kantor='$nama_kantor', no_hp='$no_hp' WHERE nik='$nik'";
-        $sql3 = "UPDATE anggrek SET nik='$nik', jenis_kelamin='$jenis_kelamin', tanggal_lahir='$tanggal_lahir', nama_tamu='$nama_tamu',
-        kota='$kota', jabatan='$jabatan', nama_kantor='$nama_kantor', no_hp='$no_hp' WHERE nik='$nik'";
-        $sql4 = "UPDATE handayani SET nik='$nik', jenis_kelamin='$jenis_kelamin', tanggal_lahir='$tanggal_lahir', nama_tamu='$nama_tamu',
-        kota='$kota', jabatan='$jabatan', nama_kantor='$nama_kantor', no_hp='$no_hp' WHERE nik='$nik'";
+
+        if(['nik2=$nik from anggrek']) {
+            $sql3 = "UPDATE anggrek 
+            SET nik2='$nik', nuptk2='$nuptk', jenis_kelamin2='$jenis_kelamin', tanggal_lahir2='$tanggal_lahir', nama_tamu2='$nama_tamu',
+            kota2='$kota', jabatan2='$jabatan', nama_kantor2='$nama_kantor', no_hp2='$no_hp' WHERE nik2='$nik'";
+        } else if(['nik=$nik from anggrek']){
+            $sql3 = "UPDATE anggrek SET nik='$nik', nuptk='$nuptk', jenis_kelamin='$jenis_kelamin', tanggal_lahir='$tanggal_lahir', nama_tamu='$nama_tamu',
+            kota='$kota', jabatan='$jabatan', nama_kantor='$nama_kantor', no_hp='$no_hp' WHERE nik='$nik'";
+        }
+
+        if(['nik2=$nik from handayani']) {
+            $sql4 = "UPDATE handayani 
+            SET nik2='$nik', nuptk2='$nuptk', jenis_kelamin2='$jenis_kelamin', tanggal_lahir2='$tanggal_lahir', nama_tamu2='$nama_tamu',
+            kota2='$kota', jabatan2='$jabatan', nama_kantor2='$nama_kantor', no_hp2='$no_hp' WHERE nik2='$nik'";
+        } else if(['nik=$nik from handayani']){
+            $sql4 = "UPDATE handayani SET nik='$nik', nuptk='$nuptk', jenis_kelamin='$jenis_kelamin', tanggal_lahir='$tanggal_lahir', nama_tamu='$nama_tamu',
+            kota='$kota', jabatan='$jabatan', nama_kantor='$nama_kantor', no_hp='$no_hp' WHERE nik='$nik'";
+        }
+
         $query1 = mysqli_query($db, $sql1);
         $query2 = mysqli_query($db, $sql2);
         $query3 = mysqli_query($db, $sql3);
