47cfd86b99b69e844a0e76b1a73eda411c09be38
AdrianSolihin <90122467+AdrianSolihin@users.noreply.github.com>
2021-12-22 09:20:08 +0700

    Add files via upload

---
+++ database/rekapan.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 22 Des 2021 pada 03.19
+-- Versi server: 10.4.21-MariaDB
+-- Versi PHP: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lpmp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `rekapan`
+--
+
+CREATE TABLE `rekapan` (
+  `id` int(110) NOT NULL,
+  `timestamp` datetime NOT NULL,
+  `nama_kegiatan` varchar(100) NOT NULL,
+  `tanggal_awal` date NOT NULL,
+  `tanggal_akhir` date NOT NULL,
+  `nama_tamu` varchar(100) NOT NULL,
+  `nik` varchar(20) NOT NULL,
+  `nuptk` varchar(20) NOT NULL,
+  `jenis_kelamin` enum('pria','wanita') NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `kota` varchar(100) NOT NULL,
+  `jabatan` varchar(100) NOT NULL,
+  `nama_kantor` varchar(100) NOT NULL,
+  `no_hp` varchar(13) NOT NULL,
+  `wisma` enum('Handayani','Anggrek') NOT NULL,
+  `nomor_kamar` int(4) NOT NULL,
+  `keterangan` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `rekapan`
+--
+ALTER TABLE `rekapan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `rekapan`
+--
+ALTER TABLE `rekapan`
+  MODIFY `id` int(110) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
