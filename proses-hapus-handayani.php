8201978c7cdabbe4b676685b2b20de2d22996164
AdrianSolihin <90122467+AdrianSolihin@users.noreply.github.com>
2021-12-22 14:04:29 +0700

    terbaru2

--- proses-hapus-handayani.php
+++ proses-hapus-handayani.php
@@ -4,6 +4,10 @@
     if(isset($_GET['nomor_kamar1'])){
         //ambil data dari formulir
         $nomor_kamar = $_GET['nomor_kamar1'];
+        $timestamp = "";
+        $nama_kegiatan = "";
+        $tanggal_awal = "";
+        $tanggal_akhir = "";
         $nik = "";
         $nuptk = "";
         $jenis_kelamin = "";
@@ -17,7 +21,7 @@
 
         //buat query update
         $sql1 = "UPDATE rekapan SET keterangan='$keterangan' where nik='$nik' AND timestamp='$timestamp'";
-        $sql = "UPDATE handayani SET 
+        $sql = "UPDATE handayani SET timestamp='$timestamp', nama_kegiatan='$nama_kegiatan', tanggal_awal='$tanggal_awal', tanggal_akhir='$tanggal_akhir',
         nik='$nik', nuptk='$nuptk', jenis_kelamin='$jenis_kelamin', tanggal_lahir='$tanggal_lahir', nama_tamu='$nama_tamu', kota='$kota', 
         jabatan='$jabatan', nama_kantor='$nama_kantor', no_hp='$no_hp' WHERE nomor_kamar='$nomor_kamar'";
 
@@ -33,6 +37,10 @@
     } else if(isset($_GET['nomor_kamar2'])) {
         //ambil data dari formulir
         $nomor_kamar = $_GET['nomor_kamar2'];
+        $timestamp = "";
+        $nama_kegiatan = "";
+        $tanggal_awal = "";
+        $tanggal_akhir = "";
         $nik = "";
         $nuptk = "";
         $jenis_kelamin = "";
@@ -46,7 +54,7 @@
 
         //buat query update
         $sql1 = "UPDATE rekapan SET keterangan='$keterangan' where nik='$nik' AND timestamp='$timestamp'";
-        $sql = "UPDATE handayani SET 
+        $sql = "UPDATE handayani SET timestamp2='$timestamp', nama_kegiatan2='$nama_kegiatan', tanggal_awal2='$tanggal_awal', tanggal_akhir2='$tanggal_akhir',
         nik2='$nik', nuptk2='$nuptk', jenis_kelamin2='$jenis_kelamin', tanggal_lahir2='$tanggal_lahir', nama_tamu2='$nama_tamu', kota2='$kota', 
         jabatan2='$jabatan', nama_kantor2='$nama_kantor', no_hp2='$no_hp' WHERE nomor_kamar='$nomor_kamar'";
 
@@ -83,9 +91,10 @@
         //buat query update
         $sql1 = "UPDATE rekapan SET keterangan='$keterangan' where nik='$nikcari' AND timestamp='$timestampcari'";
 
-        $sql = "UPDATE handayani SET timestamp='$timestamp', nama_kegiatan='$nama_kegiatan', tanggal_awal='$tanggal_awal', tanggal_akhir='$tanggal_akhir',
+        $sql = "UPDATE handyani SET timestamp='$timestamp', nama_kegiatan='$nama_kegiatan', tanggal_awal='$tanggal_awal', tanggal_akhir='$tanggal_akhir',
         nik='$nik', nuptk='$nuptk', jenis_kelamin='$jenis_kelamin', tanggal_lahir='$tanggal_lahir', nama_tamu='$nama_tamu', statusco='$statusco', kota='$kota', 
         jabatan='$jabatan', nama_kantor='$nama_kantor', no_hp='$no_hp',
+        timestamp2='$timestamp', nama_kegiatan2='$nama_kegiatan', tanggal_awal2='$tanggal_awal', tanggal_akhir2='$tanggal_akhir',
         nik2='$nik', nuptk2='$nuptk', jenis_kelamin2='$jenis_kelamin', tanggal_lahir2='$tanggal_lahir', nama_tamu2='$nama_tamu', kota2='$kota', 
         jabatan2='$jabatan', nama_kantor2='$nama_kantor', no_hp2='$no_hp' WHERE nomor_kamar='$nomor_kamar'";
 
@@ -94,7 +103,7 @@
         
         //apakah query berhasil tersimpan
         if( $query && $query1 ){
-            header('Location: pages/list-kamar/list-kamar-handayani.php');
+            header('Location: pages/list-kamar/list-kamar-handyani.php');
         } else {
             die("Gagal menyimpan perubahan...");
         }
