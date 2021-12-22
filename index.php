a0fe79426eab4ba006824954047ae5dd84eeeb6b
Sahdiki <79701880+sahdikigg@users.noreply.github.com>
2021-12-22 09:31:31 +0700

    ubah cuy

--- index.php
+++ index.php
@@ -38,7 +38,31 @@
         <a class="navbar-brand p-2 ml-md-3 ml-1" href="#">
           <img src="images/logo-lpmp.png" width="235" class="img-fluid m-0" alt="">
         </a>
+        <div class="mr-lg-5 mr-3">
+          <a class="btn btn-primary p-2 font-weight-bold" href="logout.php" onclick="return logout(event)">
+          <i class="fa fa-sign-out"></i>
+              <span class="">Logout</span>
+          </a>
+          <script type="text/javascript">
+            function logout(ev){
+              ev.preventDefault();
+              var urlToRedirect = ev.currentTarget.getAttribute('href'); 
+              console.log(urlToRedirect);
+              Swal.fire({
+              title: 'Yakin akan keluar?',
+              icon: "question",
+              showCancelButton: true,
+              confirmButtonText: 'Ya',
+              }).then((result) => {
+              if (result.isConfirmed) {
+                window.location.href = urlToRedirect;
+              }
+              })
+            }
+          </script>
+        </div>
       </nav>
+     
       <!-- partial -->
       <div class="container-fluid page-body-wrapper">
         <!-- partial -->
@@ -48,118 +72,136 @@
               <div class="col-md-12 grid-margin">
                 <div class="row">
                   <div class="col-12 col-xl-8 mb-4 mb-xl-0">
-                    <h3 class="font-weight-bold fs-30">Hai <?php echo $_SESSION['username'];?>!</h3>
-                    <h4 class="font-weight-normal mb-0">Selamat Datang <a href="http://www.lpmpkalbar.id/index.php/id/" target="blank" class=" text-decoration-none "><span class="text-primary">LPMP Kalbar</span></a> </h4>
+                    <h3 class="font-weight-bold">Hai <?php echo $_SESSION['username'];?>!</h3>
+                    <h4 class="font-weight-normal mb-0">Selamat Datang di <a href="http://www.lpmpkalbar.id/index.php/id/" target="blank" class=" text-decoration-none "><span class="text-primary">LPMP Kalbar</span></a> </h4>
                   </div>
-                  <div class="col-xl-4 pl-lg-5 d-flex justify-content-md-end pt-md-1">
-                    <div class="text-info">
-                    <?php 
+                  <div class="col-12 col-xl-4">
+                   <div class="justify-content-sm-end d-flex">
+                    <div class="dropdown flex-md-grow-1 flex-xl-grow-0">
+                      <button class="btn shadow-sm mb-3 bg-white" type="button" aria-haspopup="true" aria-expanded="true">
+                       
+                       <div class="text-info">
+                      <?php 
                     
-                    $hari = date('l');
-                    /*$new = date('l, F d, Y', strtotime($Today));*/
-                    if ($hari=="Sunday") {
-                         echo "Minggu, ";
-                    }elseif ($hari=="Monday") {
-                        echo "Senin, ";
-                    }elseif ($hari=="Tuesday") {
-                        echo "Selasa, ";
-                    }elseif ($hari=="Wednesday") {
-                        echo "Rabu, ";
-                    }elseif ($hari=="Thursday") {
-                        echo("Kamis, ");
-                    }elseif ($hari=="Friday") {
-                        echo "Jum'at, ";
-                    }elseif ($hari=="Saturday") {
-                        echo "Sabtu, ";
-                    }
-                    $tgl =date('d');
-                    echo $tgl;
-                    $bulan =date('F');
-                    if ($bulan=="January") {
-                        echo " Januari ";
-                    }elseif ($bulan=="February") {
-                        echo " Februari ";
-                    }elseif ($bulan=="March") {
-                        echo " Maret ";
-                    }elseif ($bulan=="April") {
-                        echo " April ";
-                    }elseif ($bulan=="May") {
-                        echo " Mei ";
-                    }elseif ($bulan=="June") {
-                        echo " Juni ";
-                    }elseif ($bulan=="July") {
-                        echo " Juli ";
-                    }elseif ($bulan=="August") {
-                        echo " Agustus ";
-                    }elseif ($bulan=="September") {
-                        echo " September ";
-                    }elseif ($bulan=="October") {
-                        echo " Oktober ";
-                    }elseif ($bulan=="November") {
-                        echo " November ";
-                    }elseif ($bulan=="December") {
-                        echo " Desember ";
-                    }
-                    $tahun=date('Y');
-                    echo $tahun;
+                        $hari = date('l');
+                        /*$new = date('l, F d, Y', strtotime($Today));*/
+                        if ($hari=="Sunday") {
+                              echo "Minggu, ";
+                          }elseif ($hari=="Monday") {
+                              echo "Senin, ";
+                          }elseif ($hari=="Tuesday") {
+                              echo "Selasa, ";
+                          }elseif ($hari=="Wednesday") {
+                              echo "Rabu, ";
+                          }elseif ($hari=="Thursday") {
+                              echo("Kamis, ");
+                          }elseif ($hari=="Friday") {
+                              echo "Jum'at, ";
+                          }elseif ($hari=="Saturday") {
+                              echo "Sabtu, ";
+                          }
+                          $tgl =date('d');
+                          echo $tgl;
+                          $bulan =date('F');
+                          if ($bulan=="January") {
+                              echo " Januari ";
+                          }elseif ($bulan=="February") {
+                              echo " Februari ";
+                          }elseif ($bulan=="March") {
+                              echo " Maret ";
+                          }elseif ($bulan=="April") {
+                              echo " April ";
+                          }elseif ($bulan=="May") {
+                              echo " Mei ";
+                          }elseif ($bulan=="June") {
+                              echo " Juni ";
+                          }elseif ($bulan=="July") {
+                              echo " Juli ";
+                          }elseif ($bulan=="August") {
+                              echo " Agustus ";
+                          }elseif ($bulan=="September") {
+                              echo " September ";
+                          }elseif ($bulan=="October") {
+                              echo " Oktober ";
+                          }elseif ($bulan=="November") {
+                              echo " November ";
+                          }elseif ($bulan=="December") {
+                              echo " Desember ";
+                          }
+                          $tahun=date('Y');
+                          echo $tahun;
 
-                ?>
+                        ?>
                     </div>
-                  
+                      </button>
+                    </div>
+                   </div>
                   </div>
-                          
                 </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-md-12 grid-margin">
-                <div class="card p-2">
-                  <div class="card-body">
-                    <h2 class="card-title">Buku Tamu</h2>
-                    <div class="row p-md-3">
-                      <div class="col-md-8">
+                <div class="row">
+                  <div class="col-md-12 grid-margin">
+                    <div class="row d-flex justify-content-center mt-2">
+                      <div class="col-md-7 card p-md-4 p-3 mr-md-4">
                         <div class="row">
-                          <div class="col-md-6">
-                            <div class="card shadow mb-4">
-                              <img src="images/hand-5.jpg" class="card-img-top rounded img-fluid" width="23rem" alt="...">
+                          <div class="col-md-6 mb-md-0 mb-4">
+                            <div class="card shadow" style="background-color: #09547d;">
+                              <img src="images/hand-5.jpg" class="card-img-top rounded-top img-fluid" width="23rem" alt="...">
                               <div class="card-body">
-                                <h2 class="card-title">Wisma handayani</h2>
+                                <h2 class="card-title text-white">Wisma handayani</h2>
                                 <?php $sql = "SELECT * FROM handayani";
                                       $sql1 = "SELECT * FROM handayani WHERE statusco='Kosong'";
                                       $query = mysqli_query($db, $sql);
                                       $query1 = mysqli_query($db, $sql1); ?>
-                                <p class="card-text font-weight-bold text-info mb-md-0">Total kamar : <?= mysqli_num_rows($query); ?></p>
-                                <p class="card-text font-weight-bold text-info mb-md-3">Kamar kosong : <?= mysqli_num_rows($query1); ?></p>
-                                <hr>
-                                <a href="pages/list-kamar/list-kamar-handayani.php" class="btn btn-primary">Masuk</a>
+                                <div class="row d-flex justify-content-between">
+                                  <div class="col-7">
+                                    <p class="card-text font-weight-bold mb-0 text-white">Total kamar : <?= mysqli_num_rows($query); ?></p>
+                                    <p class="card-text font-weight-bold text-white">Kamar kosong : <?= mysqli_num_rows($query1); ?></p>
+                                  </div>
+                                  <div class="col-5">
+                                    <a href="pages/list-kamar/list-kamar-handayani.php" class="btn btn-primary">Masuk</a>
+                                  </div>
+                                </div>
                               </div>
                             </div>
                           </div>
-                          <div class="col-md-6">
-                            <div class="card shadow mb-4">
-                              <img src="images/angg-1.jpg" class="card-img-top rounded img-fluid" width="23rem" alt="...">
+                          <div class="col-md-6 mb-md-0 mb-4">
+                            <div class="card shadow" style="background-color: #09547d;">
+                              <img src="images/angg-1.jpg" class="card-img-top rounded-top img-fluid" width="23rem" alt="...">
                               <div class="card-body">
-                                <h2 class="card-title">Wisma Anggrek</h2>
+                                <h2 class="card-title text-white">Wisma Anggrek</h2>
                                 <?php $sql2 = "SELECT * FROM anggrek";
                                       $sql3 = "SELECT * FROM anggrek WHERE statusco='Kosong'";
                                       $query2 = mysqli_query($db, $sql2);
                                       $query3 = mysqli_query($db, $sql3); ?>
-                                <p class="card-text font-weight-bold text-info mb-md-0">Total kamar : <?= mysqli_num_rows($query2); ?></p>
-                                <p class="card-text font-weight-bold text-info mb-md-3">Kamar kosong : <?= mysqli_num_rows($query3); ?></p>
-                                <hr>
-                                <a href="pages/list-kamar/list-kamar-anggrek.php" class="btn btn-primary">Masuk</a>
+                                <div class="row d-flex justify-content-between">
+                                  <div class="col-7">
+                                    <p class="card-text font-weight-bold mb-0 text-white">Total kamar : <?= mysqli_num_rows($query2); ?></p>
+                                    <p class="card-text font-weight-bold text-white">Kamar kosong :  <?= mysqli_num_rows($query3); ?></p>
+                                  </div>
+                                  <div class="col-5">
+                                    <a href="pages/list-kamar/list-kamar-handayani.php" class="btn btn-primary">Masuk</a>
+                                  </div>
+                                </div>
                               </div>
                             </div>
                           </div>
                         </div>
                       </div>
-                      <div class="col-md-4">
+                      <div class="col-md-4 card p-md-4">
                         <div class="row">
                           <div class="col-md-12 mb-4 stretch-card transparent" href="../../pages/list-kamar/list-kamar-handayani.php">
-                            <div class="card shadow" style="width: 18rem;">
-                              <div class="card-body hei">
+                            <div class="card shadow" style="width: 18rem; background-color: #d3d2ff;">
+                              <div class="svg d-flex justify-content-end">
+                                <div class="position-absolute ">
+                                <svg width="215" height="105" viewBox="0 0 275 165" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                  <path fill-rule="evenodd" clip-rule="evenodd" d="M137.5 165C213.439 165 275 103.439 275 27.5C275 18.0822 274.053 8.88552 272.25 0H2.7505C0.946832 8.88552 0 18.0822 0 27.5C0 103.439 61.5608 165 137.5 165Z" fill="#C4C3FF"/>
+                                  <circle cx="225" cy="137" r="28" fill="#9895FF"/>
+                                  </svg>
+                                </div>
+                              </div>
+                              <div class="card-body">
                                 <h5 class="card-title">Biodata</h5>
-                                <p class="card-text mb-5">Pengisian identitas pengunjung wisma</p>
+                                <p class="card-text mb-2">Pengisian identitas pengunjung wisma</p>
                                 <hr>
                                 <a href="pages/biodata/biodata-pengunjung.php" class="btn btn-primary">Masuk</a>
                               </div>
@@ -168,12 +210,20 @@
                         </div>
                         <div class="row">
                           <div class="col-md-12 mb-4 stretch-card transparent" href="../../pages/list-kamar/list-kamar-handayani.php">
-                            <div class="card shadow" style="width: 18rem;">
-                              <div class="card-body hei">
+                            <div class="card shadow" style="width: 18rem; background-color: #c0e2f5; ">
+                              <div class="svg d-flex justify-content-end">
+                                <div class="position-absolute ">
+                                  <svg width="215" height="105" viewBox="0 0 275 165" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M137.5 165C213.439 165 275 103.439 275 27.5C275 18.0822 274.053 8.88552 272.25 0H2.7505C0.946832 8.88552 0 18.0822 0 27.5C0 103.439 61.5608 165 137.5 165Z" fill="#91C9E9"/>
+                                    <circle cx="225" cy="137" r="28" fill="#55A3D0"/>
+                                  </svg>
+                                </div>
+                              </div>
+                              <div class="card-body">
                                 <h5 class="card-title">Rekap Pengunjung</h5>
-                                <p class="card-text mb-5">Rekapan pengunjung wisma</p>
+                                <p class="card-text mb-2">Rekapan pengunjung wisma</p>
                                 <hr>
-                                <a href="pages/rekap/rekapan.php" class="btn btn-primary ">Masuk</a>
+                                <a href="pages/rekap/rekap-anggrek.php" class="btn btn-primary mb-0">Masuk</a>
                               </div>
                             </div>
                           </div>
@@ -186,6 +236,7 @@
             </div>
             <!-- content-wrapper ends -->
             <!-- partial:partials/_footer.html -->
+
             <footer class="footer">
               <div class="d-sm-flex justify-content-center justify-content-sm-between">
                 <span class="text-muted text-center text-sm-left d-block d-sm-inline-block"
@@ -223,6 +274,7 @@
     <script src="vendors/datatables.net/jquery.dataTables.js"></script>
     <script src="vendors/datatables.net-bs4/dataTables.bootstrap4.js"></script>
     <script src="js/dataTables.select.min.js"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     <!-- End plugin js for this page -->
     <!-- inject:js -->
