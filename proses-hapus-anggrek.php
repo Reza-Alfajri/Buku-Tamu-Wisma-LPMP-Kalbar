b40e06b35b8bb70921277dd66c8feedb66586df4
Bramantio Alif Perdana <90162706+Brmsss@users.noreply.github.com>
2021-12-23 08:47:55 +0700

    Update proses-hapus-anggrek.php

--- proses-hapus-anggrek.php
+++ proses-hapus-anggrek.php
@@ -4,6 +4,11 @@
     if(isset($_GET['nomor_kamar1'])){
         //ambil data dari formulir
         $nomor_kamar = $_GET['nomor_kamar1'];
+        $sql2 = "SELECT * FROM anggrek WHERE nomor_kamar=$nomor_kamar";
+        $query2 = mysqli_query($db, $sql2);
+        $list2 = mysqli_fetch_assoc($query2);
+        $nik = $list2['nik'];
+        $timestampcari= $list2['timestamp'];
         $timestamp = "";
         $nama_kegiatan = "";
         $tanggal_awal = "";
@@ -20,7 +25,7 @@
         $keterangan = "Check Out";
 
         //buat query update
-        $sql1 = "UPDATE rekapan SET keterangan='$keterangan' where nik='$nik' AND timestamp='$timestamp'";
+        $sql1 = "UPDATE rekapan SET keterangan='$keterangan' where nik='$nik' AND timestamp='$timestampcari'";
         $sql = "UPDATE anggrek SET timestamp='$timestamp', nama_kegiatan='$nama_kegiatan', tanggal_awal='$tanggal_awal', tanggal_akhir='$tanggal_akhir',
         nik='$nik', nuptk='$nuptk', jenis_kelamin='$jenis_kelamin', tanggal_lahir='$tanggal_lahir', nama_tamu='$nama_tamu', kota='$kota', 
         jabatan='$jabatan', nama_kantor='$nama_kantor', no_hp='$no_hp' WHERE nomor_kamar='$nomor_kamar'";
@@ -37,6 +42,11 @@
     } else if(isset($_GET['nomor_kamar2'])) {
         //ambil data dari formulir
         $nomor_kamar = $_GET['nomor_kamar2'];
+        $sql2 = "SELECT * FROM anggrek WHERE nomor_kamar=$nomor_kamar";
+        $query2 = mysqli_query($db, $sql2);
+        $list2 = mysqli_fetch_assoc($query2);
+        $nik2 = $list2['nik2'];
+        $timestampcari2 = $list2['timestamp2'];
         $timestamp = "";
         $nama_kegiatan = "";
         $tanggal_awal = "";
@@ -53,7 +63,7 @@
         $keterangan = "Check Out";
 
         //buat query update
-        $sql1 = "UPDATE rekapan SET keterangan='$keterangan' where nik='$nik' AND timestamp='$timestamp'";
+        $sql1 = "UPDATE rekapan SET keterangan='$keterangan' where nik='$nik' AND timestamp='$timestampcari2'";
         $sql = "UPDATE anggrek SET timestamp2='$timestamp', nama_kegiatan2='$nama_kegiatan', tanggal_awal2='$tanggal_awal', tanggal_akhir2='$tanggal_akhir',
         nik2='$nik', nuptk2='$nuptk', jenis_kelamin2='$jenis_kelamin', tanggal_lahir2='$tanggal_lahir', nama_tamu2='$nama_tamu', kota2='$kota', 
         jabatan2='$jabatan', nama_kantor2='$nama_kantor', no_hp2='$no_hp' WHERE nomor_kamar='$nomor_kamar'";
@@ -70,8 +80,11 @@
     } else if(isset($_GET['nomor_kamar'])) {
         //ambil data dari formulir
         $nomor_kamar = $_GET['nomor_kamar'];
-        $timestampcari = $_GET['timestamp'];
-        $nikcari = $_GET['nik'];
+        $sql2 = "SELECT * FROM anggrek WHERE nomor_kamar=$nomor_kamar";
+        $query2 = mysqli_query($db, $sql2);
+        $list2 = mysqli_fetch_assoc($query2);
+        $nik = $list2['nik'];
+        $timestampcari= $list2['timestamp'];
         $timestamp = "";
         $nama_kegiatan = "";
         $tanggal_awal = "";
@@ -89,7 +102,7 @@
         $keterangan = "Check Out";
 
         //buat query update
-        $sql1 = "UPDATE rekapan SET keterangan='$keterangan' where nik='$nikcari' AND timestamp='$timestampcari'";
+        $sql1 = "UPDATE rekapan SET keterangan='$keterangan' where nik='$nik' AND timestamp='$timestampcari'";
 
         $sql = "UPDATE anggrek SET timestamp='$timestamp', nama_kegiatan='$nama_kegiatan', tanggal_awal='$tanggal_awal', tanggal_akhir='$tanggal_akhir',
         nik='$nik', nuptk='$nuptk', jenis_kelamin='$jenis_kelamin', tanggal_lahir='$tanggal_lahir', nama_tamu='$nama_tamu', statusco='$statusco', kota='$kota', 
