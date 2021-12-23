c018874c2a2ccb3ff4c8a48044f0c6d2952ec7a8
Bramantio Alif Perdana <90162706+Brmsss@users.noreply.github.com>
2021-12-23 09:22:54 +0700

    Update proses-hapus-anggrek.php

--- proses-hapus-anggrek.php
+++ proses-hapus-anggrek.php
@@ -13,7 +13,6 @@
         $nama_kegiatan = "";
         $tanggal_awal = "";
         $tanggal_akhir = "";
-        $nik = "";
         $nuptk = "";
         $jenis_kelamin = "";
         $tanggal_lahir = "";
@@ -27,7 +26,7 @@
         //buat query update
         $sql1 = "UPDATE rekapan SET keterangan='$keterangan' where nik='$nik' AND timestamp='$timestampcari'";
         $sql = "UPDATE anggrek SET timestamp='$timestamp', nama_kegiatan='$nama_kegiatan', tanggal_awal='$tanggal_awal', tanggal_akhir='$tanggal_akhir',
-        nik='$nik', nuptk='$nuptk', jenis_kelamin='$jenis_kelamin', tanggal_lahir='$tanggal_lahir', nama_tamu='$nama_tamu', kota='$kota', 
+        nik='', nuptk='$nuptk', jenis_kelamin='$jenis_kelamin', tanggal_lahir='$tanggal_lahir', nama_tamu='$nama_tamu', kota='$kota', 
         jabatan='$jabatan', nama_kantor='$nama_kantor', no_hp='$no_hp' WHERE nomor_kamar='$nomor_kamar'";
 
         $query1 = mysqli_query($db, $sql1);
@@ -45,13 +44,12 @@
         $sql2 = "SELECT * FROM anggrek WHERE nomor_kamar=$nomor_kamar";
         $query2 = mysqli_query($db, $sql2);
         $list2 = mysqli_fetch_assoc($query2);
-        $nik2 = $list2['nik2'];
-        $timestampcari2 = $list2['timestamp2'];
+        $nik = $list2['nik2'];
+        $timestampcari= $list2['timestamp2'];
         $timestamp = "";
         $nama_kegiatan = "";
         $tanggal_awal = "";
         $tanggal_akhir = "";
-        $nik = "";
         $nuptk = "";
         $jenis_kelamin = "";
         $tanggal_lahir = "";
@@ -62,10 +60,11 @@
         $no_hp = "";
         $keterangan = "Check Out";
 
+
         //buat query update
-        $sql1 = "UPDATE rekapan SET keterangan='$keterangan' where nik='$nik' AND timestamp='$timestampcari2'";
+        $sql1 = "UPDATE rekapan SET keterangan='$keterangan' where nik='$nik' AND timestamp='$timestampcari'";
         $sql = "UPDATE anggrek SET timestamp2='$timestamp', nama_kegiatan2='$nama_kegiatan', tanggal_awal2='$tanggal_awal', tanggal_akhir2='$tanggal_akhir',
-        nik2='$nik', nuptk2='$nuptk', jenis_kelamin2='$jenis_kelamin', tanggal_lahir2='$tanggal_lahir', nama_tamu2='$nama_tamu', kota2='$kota', 
+        nik2='', nuptk2='$nuptk', jenis_kelamin2='$jenis_kelamin', tanggal_lahir2='$tanggal_lahir', nama_tamu2='$nama_tamu', kota2='$kota', 
         jabatan2='$jabatan', nama_kantor2='$nama_kantor', no_hp2='$no_hp' WHERE nomor_kamar='$nomor_kamar'";
 
         $query1 = mysqli_query($db, $sql1);
@@ -83,14 +82,13 @@
         $sql2 = "SELECT * FROM anggrek WHERE nomor_kamar=$nomor_kamar";
         $query2 = mysqli_query($db, $sql2);
         $list2 = mysqli_fetch_assoc($query2);
-        $nik = $list2['nik'];
-        $timestampcari= $list2['timestamp'];
         $timestamp = "";
         $nama_kegiatan = "";
         $tanggal_awal = "";
         $tanggal_akhir = "";
         $statusco = "kosong";
-        $nik = "";
+        $nik = $list2['nik'];
+        $timestampcari= $list2['timestamp'];
         $nuptk = "";
         $jenis_kelamin = "";
         $tanggal_lahir = "";
@@ -105,10 +103,10 @@
         $sql1 = "UPDATE rekapan SET keterangan='$keterangan' where nik='$nik' AND timestamp='$timestampcari'";
 
         $sql = "UPDATE anggrek SET timestamp='$timestamp', nama_kegiatan='$nama_kegiatan', tanggal_awal='$tanggal_awal', tanggal_akhir='$tanggal_akhir',
-        nik='$nik', nuptk='$nuptk', jenis_kelamin='$jenis_kelamin', tanggal_lahir='$tanggal_lahir', nama_tamu='$nama_tamu', statusco='$statusco', kota='$kota', 
+        nik='', nuptk='$nuptk', jenis_kelamin='$jenis_kelamin', tanggal_lahir='$tanggal_lahir', nama_tamu='$nama_tamu', statusco='$statusco', kota='$kota', 
         jabatan='$jabatan', nama_kantor='$nama_kantor', no_hp='$no_hp',
         timestamp2='$timestamp', nama_kegiatan2='$nama_kegiatan', tanggal_awal2='$tanggal_awal', tanggal_akhir2='$tanggal_akhir',
-        nik2='$nik', nuptk2='$nuptk', jenis_kelamin2='$jenis_kelamin', tanggal_lahir2='$tanggal_lahir', nama_tamu2='$nama_tamu', kota2='$kota', 
+        nik2='', nuptk2='$nuptk', jenis_kelamin2='$jenis_kelamin', tanggal_lahir2='$tanggal_lahir', nama_tamu2='$nama_tamu', kota2='$kota', 
         jabatan2='$jabatan', nama_kantor2='$nama_kantor', no_hp2='$no_hp' WHERE nomor_kamar='$nomor_kamar'";
 
         $query1 = mysqli_query($db, $sql1);
