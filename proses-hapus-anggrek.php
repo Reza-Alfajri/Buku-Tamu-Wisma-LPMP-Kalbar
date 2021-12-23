6ec8642d4ae4eed36d07aea7484fc86d1e952305
Bramantio Alif Perdana <90162706+Brmsss@users.noreply.github.com>
2021-12-23 09:46:34 +0700

    Update proses-hapus-anggrek.php

--- proses-hapus-anggrek.php
+++ proses-hapus-anggrek.php
@@ -88,7 +88,9 @@
         $tanggal_akhir = "";
         $statusco = "kosong";
         $nik = $list2['nik'];
+        $nik2 = $list2['nik2'];
         $timestampcari= $list2['timestamp'];
+        $timestampcari2= $list2['timestamp2'];
         $nuptk = "";
         $jenis_kelamin = "";
         $tanggal_lahir = "";
@@ -101,7 +103,7 @@
 
         //buat query update
         $sql1 = "UPDATE rekapan SET keterangan='$keterangan' where nik='$nik' AND timestamp='$timestampcari'";
-
+        $sql3 = "UPDATE rekapan SET keterangan='$keterangan' where nik='$nik2' AND timestamp='$timestampcari2'";
         $sql = "UPDATE anggrek SET timestamp='$timestamp', nama_kegiatan='$nama_kegiatan', tanggal_awal='$tanggal_awal', tanggal_akhir='$tanggal_akhir',
         nik='', nuptk='$nuptk', jenis_kelamin='$jenis_kelamin', tanggal_lahir='$tanggal_lahir', nama_tamu='$nama_tamu', statusco='$statusco', kota='$kota', 
         jabatan='$jabatan', nama_kantor='$nama_kantor', no_hp='$no_hp',
@@ -111,9 +113,9 @@
 
         $query1 = mysqli_query($db, $sql1);
         $query = mysqli_query($db, $sql);
-        
+        $query3 = mysqli_query($db, $sql3);
         //apakah query berhasil tersimpan
-        if( $query && $query1 ){
+        if( $query && $query1 || $query3 ){
             header('Location: pages/list-kamar/list-kamar-anggrek.php');
         } else {
             die("Gagal menyimpan perubahan...");
