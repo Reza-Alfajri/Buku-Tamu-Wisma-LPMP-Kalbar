676b31a09e52c25d27fa929e207c6ae00695fd62
Bramantio Alif Perdana <90162706+Brmsss@users.noreply.github.com>
2021-12-27 09:45:59 +0700

    Add files via upload

---
+++ pages/list-admin/list-admin.php
@@ -0,0 +1,270 @@
+<?php
+	session_start();
+        if(!isset($_SESSION["username"])){
+        header("Location: ../../login.php");
+        }
+	require '../../koneksi.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>List Admin | LPMP Kalbar</title>
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="../../vendors/feather/feather.css">
+  <link rel="stylesheet" href="../../vendors/ti-icons/css/themify-icons.css">
+  <link rel="stylesheet" href="../../vendors/css/vendor.bundle.base.css">
+  <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!-- endinject -->
+  <!-- Plugin css for this page -->
+  <link rel="stylesheet" href="../../vendors/datatables.net-bs4/dataTables.bootstrap4.css">
+  <link rel="stylesheet" href="../../vendors/ti-icons/css/themify-icons.css">
+  <link rel="stylesheet" type="../../text/css" href="../../js/select.dataTables.min.css">
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="../../css/vertical-layout-light/style.css">
+  <!-- endinject -->
+  <!-- datatables -->
+  <link rel="stylesheet" href="../../dataTables/datatables.min.css">
+  <!-- end datatables -->
+  <link rel="shortcut icon" href="../../images/logo-lpmp-kecil.png" />
+  <style>
+    .swal2-popup {
+      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+  </style>
+</head>
+<body>
+  <div class="container-scroller">
+    <!-- partial:partials/_navbar.html -->
+    <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+      <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
+        <a class="navbar-brand brand-logo ms-10" href="../../index.php"><img src="../../images/logo-lpmp.png" alt="logo"/></a>
+        <a class="navbar-brand brand-logo-mini" href="../../index.php"><img src="../../images/logo-lpmp-kecil.png" alt="logo"/></a>
+      </div>
+      <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
+        <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
+          <span class="icon-menu"></span>
+        </button>
+        <!-- Start Notif -->
+        <ul class="navbar-nav navbar-nav-right">
+          
+        </ul>
+        <!-- End Notif -->
+        <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
+          <span class="icon-menu"></span>
+        </button>
+      </div>
+    </nav>
+    <!-- partial -->
+    <div class="container-fluid page-body-wrapper">
+      <!-- partial:partials/_sidebar.html -->
+      <nav class="sidebar sidebar-offcanvas" id="sidebar">
+        <ul class="nav">
+          <!-- Home -->
+          <li class="nav-item">
+            <a class="nav-link" href="../../index.php">
+              <i class="icon-grid menu-icon"></i>
+              <span class="menu-title">Home</span>
+            </a>
+          </li>
+          <!-- Biodata -->
+          <li class="nav-item">
+            <a class="nav-link" href="../../pages/biodata/biodata-pengunjung.php">
+              <i class="icon-paper menu-icon"></i>
+              <span class="menu-title">Biodata Tamu</span>
+            </a>
+          </li>
+         <!-- List Kamar -->
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#list-kamar" aria-expanded="false" aria-controls="list-kamar">
+              <i class="icon-layout menu-icon"></i>
+              <span class="menu-title">List Kamar</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="list-kamar">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="../../pages/list-kamar/list-kamar-handayani.php">Handayani</a></li>
+                <li class="nav-item"> <a class="nav-link" href="../../pages/list-kamar/list-kamar-anggrek.php">Anggrek</a></li>
+              </ul>
+            </div>
+          </li>
+          <!-- Rekapan -->
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#rekapan-tamu" aria-expanded="false" aria-controls="rekapan-tamu">
+              <i class="ti-agenda menu-icon"></i>
+              <span class="menu-title">Rekapan Tamu</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="rekapan-tamu">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="../../pages/rekap/rekap-handayani.php">Handayani</a></li>
+                <li class="nav-item"> <a class="nav-link" href="../../pages/rekap/rekap-anggrek.php">Anggrek</a></li>
+              </ul>
+            </div>
+          </li>
+          <!-- LIST ADMIN -->
+          <?php
+             $a= "SELECT * FROM admin WHERE username='$_SESSION[username]'";
+             $admin= mysqli_query($db, $a);
+             $adm=mysqli_fetch_array($admin);
+             if ($adm['level'] == 'admin') {
+                $konten = '<li class="nav-item">
+                <a class="nav-link" href="../../pages/list-admin/list-admin.php" hidden="hidden">
+                  <i class="icon-paper menu-icon"></i>
+                  <span class="menu-title">List Admin</span>
+                </a>
+              </li>';
+             } else {
+              $konten = '<li class="nav-item active">
+              <a class="nav-link" href="../../pages/list-admin/list-admin.php">
+                <i class="icon-paper menu-icon"></i>
+                <span class="menu-title">List Admin</span>
+              </a>
+            </li>';
+             }
+             echo $konten;
+          ?>
+          <!-- Logout -->
+          <li class="nav-item">
+            <a class="nav-link" href="../../logout.php" onclick="return logout(event)">
+              <i class="fa fa-sign-out menu-icon"></i>
+              <span class="menu-title">Logout</span>
+            </a>
+          </li>
+          <script type="text/javascript">
+            function logout(ev){
+              ev.preventDefault();
+              var urlToRedirect = ev.currentTarget.getAttribute('href'); 
+              console.log(urlToRedirect);
+              Swal.fire({
+              title: 'Yakin akan keluar?',
+              icon: "question",
+              showCancelButton: true,
+              confirmButtonText: 'Ya',
+              }).then((result) => {
+              if (result.isConfirmed) {
+                window.location.href = urlToRedirect;
+              }
+              })
+            }
+          </script>
+          <!--  -->
+        </ul>
+      </nav>
+      <!-- partial -->
+      <div class="main-panel">
+        <div class="content-wrapper">
+          <div class="row">
+            <div class="col-md-12 grid-margin">
+              <div class="row">
+                <div class="col-12 col-xl-8 mb-4 mb-xl-0">
+                  <h3 class="font-weight-bold">List Admin Wisma</h3>
+                </div>
+              </div> 
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12 grid-margin">
+              <div class="card">
+                <div class="card-body">
+                    <!-- Kodingan Isi Halaman Page Di Bawah Ini -->
+                    <!-- Tabel -->
+                    <div class="row">
+                      <div class="col-md-12" style="overflow-x: auto">
+                        <div class="table-responsive" style="width:auto">
+                          <table class="table" id="TableBiodata">
+                              <thead class="thead-dark">
+                                <tr>
+                                    <th>Username</th>
+                                    <th>Password</th>
+                                    <th>&nbsp;</th>
+                                </tr>
+                              </thead>
+                              <tbody>
+                                <!-- Search -->
+                                <?php
+                                    $sql = "SELECT * FROM admin WHERE level='admin'";
+                                    $sql1 = "SELECT * FROM admin WHERE level='admin'";
+                                    $query = mysqli_query($db, $sql);
+                                    $query1 = mysqli_query($db, $sql1);
+                                ?>
+                                <!-- End Search -->
+                                <?php while($list=mysqli_fetch_array($query1)) : 
+                                  //$no++; 
+                                ?> 
+                                  <tr class="alert" role="alert">
+                                    <td><?= $list['username']; ?></td>
+                                    <td><?= $list['password']; ?></td>
+                                    <td>                               
+                                    <a href=../../proses-hapus-admin.php?username=".$list['username']." onclick='return hapus(event)'>
+                                    <span aria-hidden='true'><i class='fa fa-times-circle'></i></span>Delete
+                                    </a>
+                                  </tr>
+                                <?php endwhile; ?>
+                            </tbody>
+                        </table>
+                        </div>
+                      </div>
+                    </div>
+                    <!-- End Tabel -->
+                </div>
+              </div>
+            </div>
+          </div>
+        <!-- content-wrapper ends -->
+        <!-- partial:partials/_footer.html -->
+        <footer class="footer">
+          <div class="d-sm-flex justify-content-center justify-content-sm-between">
+            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2021.  Premium <a href="https://www.bootstrapdash.com/" target="_blank">Bootstrap admin template</a> from BootstrapDash. All rights reserved.</span>
+            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="ti-heart text-danger ml-1"></i></span>
+          </div>
+          <div class="d-sm-flex justify-content-center justify-content-sm-between">
+            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Distributed by <a href="https://www.themewagon.com/" target="_blank">Themewagon</a></span> 
+            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Edited by interns from Polnep | 2021</span>  
+	  </div>
+        </footer> 
+        <!-- partial -->
+      </div>
+      <!-- main-panel ends -->
+    </div>   
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+
+  <!-- plugins:js -->
+  <script src="../../vendors/js/vendor.bundle.base.js"></script>
+  <!-- endinject -->
+
+  <!-- Plugin js for this page -->
+  <script src="../../vendors/chart.js/Chart.min.js"></script>
+  <script src="../../vendors/datatables.net/jquery.dataTables.js"></script>
+  <script src="../../vendors/datatables.net-bs4/dataTables.bootstrap4.js"></script>
+  <script src="../../js/dataTables.select.min.js"></script>
+  <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+  <!-- End plugin js for this page -->
+  
+  <!-- inject:js -->
+  <script src="../../js/off-canvas.js"></script>
+  <script src="../../js/hoverable-collapse.js"></script>
+  <script src="../../js/template.js"></script>
+  <script src="../../js/settings.js"></script>
+  <script src="../../js/todolist.js"></script>
+  <!-- endinject -->
+  <!-- Custom js for this page-->
+  <script src="../../js/dashboard.js"></script>
+  <script src="../../js/Chart.roundedBarCharts.js"></script>
+  <!-- End custom js for this page-->
+  <script type="text/javascript">
+    $(document).ready( function () {
+    $('#TableBiodata').DataTable();
+    } );
+  </script>
+  <!-- end datatabels -->
+</body>
+</html>
