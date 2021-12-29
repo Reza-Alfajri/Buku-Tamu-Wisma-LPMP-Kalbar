ee13926b68acec43b812476b7229485548cce01e
AdrianSolihin <90122467+AdrianSolihin@users.noreply.github.com>
2021-12-29 12:56:20 +0700

    terbaru2

--- proses-edit-biodata.php
+++ proses-edit-biodata.php
@@ -20,30 +20,50 @@
         $sql2 = "UPDATE rekapan SET nik='$nik', nuptk='$nuptk', jenis_kelamin='$jenis_kelamin', tanggal_lahir='$tanggal_lahir', nama_tamu='$nama_tamu',
         kota='$kota', jabatan='$jabatan', nama_kantor='$nama_kantor', no_hp='$no_hp' WHERE nik='$nik' or nama_tamu='$nama_tamu' or nuptk='$nuptk'";
 
-        $sq15 = "SELECT * FROM anggrek WHERE nik2=$nik or nama_tamu2='$nama_tamu' or nuptk2='$nuptk'";
-        if($sql5) {
+        $sql5 = "SELECT * FROM anggrek WHERE nik2=$nik or nama_tamu2='$nama_tamu' or nuptk2='$nuptk'";
+        $query5 = mysqli_query($db, $sql5);
+        if($query5 ) {
             $sql3 = "UPDATE anggrek SET nik2='$nik', nuptk2='$nuptk', jenis_kelamin2='$jenis_kelamin', tanggal_lahir2='$tanggal_lahir', nama_tamu2='$nama_tamu',
             kota2='$kota', jabatan2='$jabatan', nama_kantor2='$nama_kantor', no_hp2='$no_hp' WHERE nik2='$nik' or nama_tamu2='$nama_tamu' or nuptk2='$nuptk'";
+            $query3 = mysqli_query($db, $sql3);
         } else {
+            die("Gagal menyimpan perubahan a...");
+        }  
+        
+        $sql6 = "SELECT * FROM anggrek WHERE nik=$nik or nama_tamu='$nama_tamu' or nuptk='$nuptk'";
+        $query6 = mysqli_query($db, $sql6);
+        if($query6) {
             $sql3 = "UPDATE anggrek SET nik='$nik', nuptk='$nuptk', jenis_kelamin='$jenis_kelamin', tanggal_lahir='$tanggal_lahir', nama_tamu='$nama_tamu',
             kota='$kota', jabatan='$jabatan', nama_kantor='$nama_kantor', no_hp='$no_hp' WHERE nik='$nik' or nama_tamu='$nama_tamu' or nuptk='$nuptk'";
-        }
+            $query3 = mysqli_query($db, $sql3);
+        } else {
+            die("Gagal menyimpan perubahan b...");
+        }  
 
-        $sq16 = "SELECT * FROM handayani WHERE nik2=$nik or nama_tamu2='$nama_tamu' or nuptk2='$nuptk'";
-        if($sq16) {
+        $sql7 = "SELECT * FROM handayani WHERE nik2=$nik or nama_tamu2='$nama_tamu' or nuptk2='$nuptk'";
+        $query7 = mysqli_query($db, $sql7);
+        if($query7 ) {
             $sql4 = "UPDATE handayani SET nik2='$nik', nuptk2='$nuptk', jenis_kelamin2='$jenis_kelamin', tanggal_lahir2='$tanggal_lahir', nama_tamu2='$nama_tamu',
             kota2='$kota', jabatan2='$jabatan', nama_kantor2='$nama_kantor', no_hp2='$no_hp' WHERE nik2='$nik' or nama_tamu2='$nama_tamu' or nuptk2='$nuptk'";
+            $query4 = mysqli_query($db, $sql4);
         } else {
+            die("Gagal menyimpan perubahan c...");
+        }  
+        
+        $sql8 = "SELECT * FROM handayani WHERE nik=$nik or nama_tamu='$nama_tamu' or nuptk='$nuptk'";
+        $query8 = mysqli_query($db, $sql8);
+        if($query8) {
             $sql4 = "UPDATE handayani SET nik='$nik', nuptk='$nuptk', jenis_kelamin='$jenis_kelamin', tanggal_lahir='$tanggal_lahir', nama_tamu='$nama_tamu',
             kota='$kota', jabatan='$jabatan', nama_kantor='$nama_kantor', no_hp='$no_hp' WHERE nik='$nik' or nama_tamu='$nama_tamu' or nuptk='$nuptk'";
-        }
+            $query4 = mysqli_query($db, $sql4);
+        } else {
+            die("Gagal menyimpan perubahan d...");
+        }  
 
         $query1 = mysqli_query($db, $sql1);
         $query2 = mysqli_query($db, $sql2);
-        $query3 = mysqli_query($db, $sql3);
-        $query4 = mysqli_query($db, $sql4);
         //apakah query berhasil tersimpan
-        if( $query1 && $query2 && $query3 && $query4 ){
+        if( $query1 && $query2 && $query3 || $query4 ){
             header('Location: pages/biodata/biodata-pengunjung.php');
         } else {
             die("Gagal menyimpan perubahan ...");
