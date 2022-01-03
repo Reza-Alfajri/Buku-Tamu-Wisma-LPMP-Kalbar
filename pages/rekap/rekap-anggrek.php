0575bfe066039e4176fd5af70b6151f921f8f194
Sahdiki <79701880+sahdikigg@users.noreply.github.com>
2022-01-03 09:32:46 +0700

    fix bro

--- pages/rekap/rekap-anggrek.php
+++ pages/rekap/rekap-anggrek.php
@@ -181,7 +181,7 @@
                       <div class="col-md-12" style="overflow-x: auto">
                         <div class="table-responsive" style="width:auto">
                           <table class="table">
-                              <thead class="thead-dark">
+                              <thead class="text-white" style="background-color: #4FAFC3;">
                                 <tr>
                                 <th>Timestamp</th>
                                 <th>Nama Kegiatan</th>
@@ -425,13 +425,13 @@
           </div>  
         <!-- content-wrapper ends -->
         <!-- partial:partials/_footer.html -->
-        <footer class="footer">
-          <div class="d-sm-flex justify-content-center justify-content-sm-between">
-            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2021.  Premium <a href="https://www.bootstrapdash.com/" target="_blank">Bootstrap admin template</a> from BootstrapDash. All rights reserved.</span>
+        <footer class="footer" style="background-color: #09547d;">
+          <div class="d-sm-flex justify-content-center justify-content-sm-between text-white">
+            <span class=" text-center text-sm-left d-block d-sm-inline-block">Copyright © 2021.  Premium <a href="https://www.bootstrapdash.com/" target="_blank">Bootstrap admin template</a> from BootstrapDash. All rights reserved.</span>
             <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="ti-heart text-danger ml-1"></i></span>
           </div>
-          <div class="d-sm-flex justify-content-center justify-content-sm-between">
-            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Distributed by <a href="https://www.themewagon.com/" target="_blank">Themewagon</a></span> 
+          <div class="d-sm-flex justify-content-center justify-content-sm-between text-white">
+            <span class=" text-center text-sm-left d-block d-sm-inline-block">Distributed by <a href="https://www.themewagon.com/" target="_blank">Themewagon</a></span> 
             <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Edited by interns from Polnep | 2021</span>
 	  </div>
         </footer> 
